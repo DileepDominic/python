9eee4cda7cf0d388275c8bac16887ed2f4e03efc
Dileep R Dominic <DileepDominic@users.noreply.github.com>
2018-09-05 23:21:16 +0530

    Create list comprehension.py

---
+++ list comprehension.py
@@ -0,0 +1,18 @@
+    x = 1
+    y = 1
+    z = 1
+    n = 1
+
+    list1 = [x, y, z]
+    list2 = [[a, b, c]
+                for a in range(x+1)
+                for b in range(y+1)
+                for c in range(z+1)     
+            ]
+    print(list1)
+    
+############################
+This output gives a permutation of values for easch 
+
+    
+    
