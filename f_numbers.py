e81721575fde9d525d39834399c5e58b4fe05c5f
dileepdominic <dileepdominic@gmail.com>
2020-03-25 20:53:37 +0530

    Added f_numbers.py

---
+++ f_numbers.py
@@ -0,0 +1,31 @@
+#integer
+intnum = 10
+print(intnum)
+
+intnum = -10
+print(intnum)
+
+#floating point number
+floatnum = 1.00001
+print(floatnum)
+
+floatnum = -1.00001
+print(floatnum)
+#-1.00001
+
+#complex number
+complexnum = complex(1,2)
+print(complexnum)
+
+complexnum = complex(0,2)
+print(complexnum)
+
+complexnum = complex(1,0)
+print(complexnum)
+
+complexnum = complex(1.5,2.3)
+print(complexnum)
+
+#(1.5+2.3j)
+
+
