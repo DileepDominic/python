25e16820caf5bbc33894635e261913f2fc1bbd83
dileepdominic <dileepdominic@gmail.com>
2020-08-11 23:13:56 +0530

    Updted single linked list

---
+++ datastructures/singleLinkedList.py
@@ -0,0 +1,36 @@
+class node(object):
+    def __init__(self,value):
+        self.value = value
+        self.next = next_node
+    
+    def __repr__(self):
+        next_value = self.next and self.next.value or None
+        return f"[{self.value}:{repr(next_value)}]"
+
+
+class singleLinkedList(object):
+    def __init__(self):
+        self.first = None
+        self.last = None
+
+    def push(self,obj_node):
+    
+    def pop(self):
+
+    def shift(self,obj_node):
+    
+    def unshift(self):
+
+    def remove(self,obj_node):
+    
+    def first(self):
+
+    def last(self):
+    
+    def count(self):
+    
+    def get(self,index):
+    
+    def dump(self,mark):
+
+
